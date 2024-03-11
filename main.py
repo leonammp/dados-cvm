9127355b437a43c23dc95965419a8b04087c5dc7
Leonam Mendonça <leonam.pereira@finnet.com.br>
2024-03-11 18:18:42 -0300

    add flask

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import time
 
+from flask import Flask
+from gevent import monkey;
+
+monkey.patch_all()
+import gevent
 from dotenv import load_dotenv
 import schedule
 
@@ -7,6 +12,7 @@ from db.db import DB
 from services.cvmDataService import CVMDataService
 
 load_dotenv()
+app = Flask(__name__)
 
 url_arquivo = 'https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/IPE/DADOS/ipe_cia_aberta_2024.zip'
 extrair_para = 'temp'
@@ -22,6 +28,22 @@ def executar_servico_cvm():
 # Agendar a execução do serviço todos os dias às 01h
 schedule.every().day.at("01:00").do(executar_servico_cvm)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
+# Definir uma rota para verificar o status do serviço
+@app.route("/")
+def status():
+    return "Serviço CVM agendado para ser executado todos os dias às 01h."
+
+
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        gevent.sleep(1)
+
+
+if __name__ == "__main__":
+    # Criar uma nova tarefa Gevent para executar o agendamento
+    schedule_task = gevent.spawn(run_schedule)
+
+    # Iniciar o servidor Flask assíncrono com Gevent
+    app.run()
